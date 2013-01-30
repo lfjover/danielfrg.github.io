7c068f7278cad89b8fe177789ac3b5a142cc1d46
Daniel Rodriguez <df.rodriguez143@gmail.com>
2013-01-30 00:44:12 -0600

    fix sitemap

--- source/_plugins/generate_sitemap.rb
+++ source/_plugins/generate_sitemap.rb
@@ -40,7 +40,7 @@ require 'rexml/document'
 module Jekyll
 
   # Change MY_URL to reflect the site you are using
-  MY_URL = "http://danielfrg.github.com"
+  MY_URL = "http://danielfrg.github.com/"
 
   # Change SITEMAP_FILE_NAME if you would like your sitemap file
   # to be called something else
