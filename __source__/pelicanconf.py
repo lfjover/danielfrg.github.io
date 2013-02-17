926ab30033e05455bf4a65b880e3e5fc41410ec1
Daniel Rodriguez <df.rodriguez143@gmail.com>
2013-02-17 10:20:13 -0600

    Pelican version

---
+++ __source__/pelicanconf.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+SITEURL = 'http://danielfrg.github.com'
+AUTHOR = u'Daniel Rodriguez'
+SITENAME = u'Daniel Rodriguez'
+TIMEZONE = 'America/Regina'
+DEFAULT_LANG = u'en'
+DEFAULT_PAGINATION = 5
+MARKUP = ('md', 'ipynb')
+
+THEME = "./themes/danielfrg"
+PLUGINS = ['pelican.plugins.assets', 'pelican.plugins.sitemap']
+STATIC_PATHS = ["images", ]
+
+GOOGLE_ANALYTICS = 'UA-35523657-2'
+
+ARTICLE_URL = 'blog/{date:%Y}/{date:%m}/{date:%d}/{slug}/'
+ARTICLE_SAVE_AS = 'blog/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
+
+PAGE_URL = '/{slug}'
+PAGE_SAVE_AS = '/{slug}/index.html'
+
+FILES_TO_COPY = (('robots.txt', 'robots.txt'),
+                 ('googleaa9959ae757fe3e1.html', 'googleaa9959ae757fe3e1.html'),
+                 ('favicon.ico', 'favicon.ico'),)
+
--- _source/_utils/fabfile.py
+++
@@ -1,9 +0,0 @@
-import os
-import shutil
-import distutils
-from fabric.api import local
-
-
-def convert(iPythonFile):
-    os.system("python nbconvert/jekyll.py %s" % iPythonFile)
-
