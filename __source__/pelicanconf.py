7333c19ae49119b3a1e6531229831047bc695521
Daniel Rodriguez <df.rodriguez143@gmail.com>
2013-03-19 01:10:18 -0500

    add 404 page and fix css

--- __source__/pelicanconf.py
+++ __source__/pelicanconf.py
@@ -24,10 +24,11 @@ GOOGLE_ANALYTICS = 'UA-35523657-2'
 ARTICLE_URL = 'blog/{date:%Y}/{date:%m}/{date:%d}/{slug}/'
 ARTICLE_SAVE_AS = 'blog/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
 
-PAGE_URL = '/{slug}'
-PAGE_SAVE_AS = '/{slug}/index.html'
+PAGE_URL = '/{slug}.html'
+PAGE_SAVE_AS = '/{slug}.html'
 
 FILES_TO_COPY = (('robots.txt', 'robots.txt'),
                  ('googleaa9959ae757fe3e1.html', 'googleaa9959ae757fe3e1.html'),
-                 ('favicon.ico', 'favicon.ico'),)
+                 ('favicon.ico', 'favicon.ico'),)   
+                 # ('404.html', '404.html'),)
 
