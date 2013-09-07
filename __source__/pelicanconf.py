6ff364c260cbf28909ea471753a61693956c730b
Daniel Rodriguez <drodriguez@oculus360.us>
2013-09-07 11:58:36 -0500

    With new theme

--- __source__/pelicanconf.py
+++ __source__/pelicanconf.py
@@ -4,9 +4,8 @@ SITEURL = '/'
 SITEURL = 'http://danielfrg.github.io'
 AUTHOR = 'Daniel Rodriguez'
 SITENAME = 'Daniel Rodriguez'
-TIMEZONE = 'America/Regina'
+TIMEZONE = 'UTC'
 DEFAULT_LANG = 'en'
-DEFAULT_PAGINATION = 10
 MARKUP = ('md', 'ipynb')
 DEFAULT_DATE_FORMAT = '%d.%m.%Y'
 
@@ -24,8 +23,6 @@ SITEMAP = {
 PLUGIN_PATH = './plugins'
 PLUGINS = ['sitemap', 'ipythonnb']
 
-GOOGLE_ANALYTICS = 'UA-35523657-2'
-
 ARTICLE_URL = 'blog/{date:%Y}/{date:%m}/{date:%d}/{slug}/'
 ARTICLE_SAVE_AS = 'blog/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
 
@@ -33,3 +30,14 @@ FILES_TO_COPY = (('robots.txt', 'robots.txt'),
                  ('404.html', '404.html'),
                  ('favicon.ico', 'favicon.ico'),
                  ('google_verification.html', 'googleaa9959ae757fe3e1.html'),)
+
+# Theme settings
+HOMEPAGE_INTRO = "This blog is written by Daniel Rodriguez. He writes about coding, python, data science and his projects"
+
+TWITTER_USERNAME = "danielfrg"
+GITHUB_USERNAME = "danielfrg"
+EMAIL_ADDRESS = "df.rodriguez143@gmail.com"
+SHOW_FEED = True
+
+GOOGLE_ANALYTICS = 'UA-35523657-2'
+DISQUS_USERNAME = 'danielfrg'
--- theme/js/social.js
+++ theme/js/social.js
@@ -39,12 +39,3 @@
     );
 }(document, 'script'));
 
-/* DISQUS */
-/* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
-var disqus_shortname = 'danielfrg'; // required: replace example with your forum shortname
-/* * * DON'T EDIT BELOW THIS LINE * * */
-(function() {
-    var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-    dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
-    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-})();
